4df5e571e49928102b3bd58c8cb273807ef10997
yada2x <lawrenceltnguyen@gmail.com>
2025-01-23 22:40:39 +1100

    Set up project with SDL3

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Temp", WIDTH, HEIGHT, SDL_WINDOW_HIGH_PIXEL_DENSITY);
+
+    if (NULL == window) {
+        std::cout << "Could not create window: " << SDL_GetError << std::endl;
+    }
+
+    SDL_Event windowEvent;
+
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (SDL_EVENT_QUIT == windowEvent.type) {
+                break;
+            }
+        }
+    }
+    
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
+}
